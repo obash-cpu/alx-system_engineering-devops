b1953781ff9e75ce13a60fe9584b0511da25f671
obash-cpu <absalomobala@gmail.com>
2023-04-23 05:06:09 -0700

    task

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
-class ssh_config {
-  file { '/home/ubuntu/.ssh/config':
-    ensure => 'file',
-    owner => 'ubuntu',
-    group => 'ubuntu',
-    mode => '0600',
-    content => "
-      Host 164533-web-01
-        IdentityFile ~/.ssh/school
-        PasswordAuthentication no
-    ",
-  }
+# Setting up my client config file
+include stdlib
+
+file_line { 'Turn off passwd auth':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  replace => true,
+}
+
+file_line { 'Delare identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '     IdentityFile ~/.ssh/school',
+  replace => true,
 }
