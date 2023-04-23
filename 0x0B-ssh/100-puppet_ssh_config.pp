f67175ade28cdd35bf3888dfd5e885c8c98c29f6
obash-cpu <absalomobala@gmail.com>
2023-04-23 05:03:03 -0700

    task correction

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 class ssh_config {
-  file { '/home/<your_username>/.ssh/config':
+  file { '/home/ubuntu/.ssh/config':
     ensure => 'file',
-    owner => '<your_username>',
-    group => '<your_username>',
+    owner => 'ubuntu',
+    group => 'ubuntu',
     mode => '0600',
     content => "
-      Host <your_server_hostname>
+      Host 164533-web-01
         IdentityFile ~/.ssh/school
         PasswordAuthentication no
     ",
