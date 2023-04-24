138c6ebdb9467f3442f2e40e9db91ab45ac0ebd2
obash-cpu <absalomobala@gmail.com>
2023-04-24 02:29:36 -0700

    pkill task

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # puppet command to kill a process using pkill and exec
-exec {  'kill myprocess':
-  command => '/usr/bin/pkill myprocess',
-  path    => ['/usr/bin', '/usr/local/bin'],
+exec { 'pkill':
+  command  => 'pkill killmenow',
+  provider => 'shell',
 }
